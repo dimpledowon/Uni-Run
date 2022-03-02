172153fd5029b9696de890f856523a9d8f962ba3
dimpledowon <dowon1102@naver.com>
2022-03-02 12:40:38 +0900

    20220302
    Uni-Run

--- Assets/02.Scrips/ScrollingObject.cs
+++ Assets/02.Scrips/ScrollingObject.cs
@@ -4,8 +4,12 @@
 public class ScrollingObject : MonoBehaviour {
     public float speed = 10f; // 이동 속도
 
-    private void Update() {
-        // 초당 speed의 속도로 왼쪽으로 평행이동
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    private void Update() 
+    {
+        if (!GameManager.instance.isGameover)
+        {
+            // 초당 speed의 속도로 왼쪽으로 평행이동
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
     }
 }
